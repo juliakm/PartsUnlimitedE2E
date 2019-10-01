9eed8a005d87d8f67e87728967277ebe1aea6cd0
Julia Kulla-Mader <julia.kullamader@microsoft.com>
2019-10-01 10:46:14 -0400

    Cleaning up

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    //Second comment
     public class Category
     {
         public int CategoryId { get; set; }
